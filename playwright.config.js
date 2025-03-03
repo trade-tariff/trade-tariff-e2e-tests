5c1a6e3b6693a10289222eef008efaccd426af91
William Fish <william.michael.fish@gmail.com>
2025-03-03 16:27:40 +0000

    BAU: Migrate to javascript

---
+++ playwright.config.js
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+import path from "path";
+
+const playwrightEnv = process.env.PLAYWRIGHT_ENV ?? "development";
+const envFile = path.resolve(__dirname, `.env.${playwrightEnv}`);
+dotenv.config({ path: envFile });
+
+// See https://playwright.dev/docs/test-configuration.
+const onCI = (process.env.CI ?? "false") === "true";
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: onCI,
+  retries: onCI ? 2 : 0,
+  workers: onCI ? 1 : undefined,
+  reporter: "html",
+  use: { trace: "on" },
+  // TODO: Set the timeout that makes sense for the OTT project
+  // timeout: 140000,
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});

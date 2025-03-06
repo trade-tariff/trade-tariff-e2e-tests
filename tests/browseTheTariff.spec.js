811551d96210680d2a1b61383d1d730789eea4b3
William Fish <william.michael.fish@gmail.com>
2025-03-06 14:42:59 +0000

    HMRC-801: Code review

--- tests/browseTheTariff.spec.js
+++ tests/browseTheTariff.spec.js
@@ -1,21 +1,14 @@
 const { test, expect } = require('@playwright/test');
-import { baseUrl } from "../playwright-variables";
 
 test.describe("BrowserPage", () => {
-  test.describe.configure({ mode: "serial" });
+  test("Validating the browserPageFunctionality", async ({ page }) => {
+    await page.goto('/browse');
 
-  test.beforeEach(async ({ page }) => {
-    await page.goto(baseUrl);
-  });
-  
-    test("Validating the browserPageFunctionality", async ({ page }) => {
-        await page.getByRole('link', { name: 'Section 1:   Live animals;' }).click();
-        await page.getByRole('link', { name: 'Chapter 01:   Live animals' }).click();
-        await page.getByRole('link', { name: 'Heading 01:   Live horses,' }).click();
-        await page.getByRole('link', { name: 'Commodity code 0101210000,' }).click();
-        await expect(page.getByLabel('Breadcrumb').getByText('Commodity')).toBeVisible();
-        await page.close();
-    })
+    await page.getByRole('link', { name: 'Section 1:   Live animals;' }).click();
+    await page.getByRole('link', { name: 'Chapter 01:   Live animals' }).click();
+    await page.getByRole('link', { name: 'Heading 01:   Live horses,' }).click();
+    await page.getByRole('link', { name: 'Commodity code 0101210000,' }).click();
+
+    await expect(page.getByLabel('Breadcrumb').getByText('Commodity')).toBeVisible();
   })
-
-
+})

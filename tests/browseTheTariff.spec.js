4f19f4dd79f9b611aadea45f46aec1314b5c0c3d
Sandya25design <Sandya.karra@transformuk.com>
2025-03-11 09:36:30 +0000

    Adding a-z navigation test case to the repo

--- tests/browseTheTariff.spec.js
+++ tests/browseTheTariff.spec.js
@@ -3,11 +3,15 @@ const { test, expect } = require('@playwright/test');
 test.describe("BrowserPage", () => {
   test("Validating browsing the tariff", async ({ page }) => {
     await page.goto('/browse');
-    await page.getByRole('link', { name: 'Section 1:   Live animals;' }).click();
-    await page.getByRole('link', { name: 'Chapter 01:   Live animals' }).click();
-    await page.getByRole('link', { name: 'Heading 01:   Live horses,' }).click();
+    await page.getByRole('link', { name: 'Section 1: Live animals;' }).click();
+    await page.getByRole('link', { name: 'Chapter 01: Live animals' }).click();
+    await page.getByRole('link', { name: 'Heading 01: Live horses,' }).click();
     await page.getByRole('link', { name: 'Commodity code 0101210000,' }).click();
     await expect(page.getByLabel('Breadcrumb').getByText('Commodity')).toBeVisible();
-
-  })
+  });
+  
+  test.afterEach(async({ page })=> {
+    await page.close();
+  });
+  
 })
--- tests/searchTheTariff.spec.js
+++ tests/searchTheTariff.spec.js
@@ -6,6 +6,13 @@ test.describe("SearchPage", () => {
     await page.getByRole('combobox', { name: 'Search the UK Integrated' }).click();
     await page.getByRole('combobox', { name: 'Search the UK Integrated' }).fill('clothes');
     await page.getByRole('combobox', { name: 'Search the UK Integrated' }).press('Enter');
-    await expect(page.getByRole('heading', { name: 'Results matching ‘clothes’' })).toBeVisible();
-  })
+    await expect(page.getByRole("heading", { name: "Search results for ‘clothes’" })).toBeVisible();
+    await page.getByRole("link", { name: "Hand-made (code 6217100010)" }).click();
+    await expect(page.getByLabel('Breadcrumb').getByText('Commodity')).toBeVisible();
+  });
+  
+  test.afterEach(async ({ page }) => {
+    await page.close();
+  });
+
 })

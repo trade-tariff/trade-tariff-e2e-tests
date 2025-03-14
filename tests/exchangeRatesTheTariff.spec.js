3e6667179fd8a074b94c2af610fac95bc6f0b81f
Sandya25design <Sandya.karra@transformuk.com>
2025-03-14 15:23:45 +0000

    Added test for exchange rates in the tariff module

---
+++ tests/exchangeRatesTheTariff.spec.js
@@ -0,0 +1,12 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe("Foreign currency exchange rates", () => {
+  test("Calculate customs value using exchange rates", async ({ page }) => {
+    await page.goto("/exchange_rates");
+    await expect(page.getByRole('heading', { name: 'Check foreign currency' })).toBeVisible();
+    await page.getByRole('link', { name: 'Currency exchange average' }).click();
+    await expect(page.getByRole('heading', { name: 'HMRC currency exchange' })).toBeVisible();
+    await page.getByRole('link', { name: 'Currency exchange spot rates' }).click();
+    await expect(page.getByRole('heading', { name: 'HMRC currency exchange spot' })).toBeVisible();  
+  });
+});

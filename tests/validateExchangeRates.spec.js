bd1dbcdd550b5e6cd14eb51d2f37983554ee414d
Sandya25design <Sandya.karra@transformuk.com>
2025-03-25 14:05:22 +0000

    Journey- extend the exchange rates checks to include loading the view online page

--- tests/validateExchangeRates.spec.js
+++ tests/validateExchangeRates.spec.js
@@ -6,7 +6,15 @@ test.describe("Exchange Rates", () => {
     await expect(page.getByRole('heading', { name: 'Check foreign currency' })).toBeVisible();
     await page.getByRole('link', { name: 'Currency exchange average' }).click();
     await expect(page.getByRole('heading', { name: 'HMRC currency exchange' })).toBeVisible();
+    await page.getByTitle("View 12 2024 monthly exchange").click();
+    await expect(page.getByRole("columnheader", { name: "Country/territory" })).toBeVisible();
+    await expect(page.getByRole("columnheader", { name: "Currency units per £" })).toBeVisible();
+    await page.getByRole("link", { name: "Average exchange rates" }).click();
     await page.getByRole('link', { name: 'Currency exchange spot rates' }).click();
     await expect(page.getByRole('heading', { name: 'HMRC currency exchange spot' })).toBeVisible();
+    await page.getByTitle("View 12 2024 monthly exchange").click();
+    await expect(page.getByRole("columnheader", { name: "Currency units per £" })).toBeVisible();
+    await expect(page.getByRole("columnheader", { name: "Country/territory" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "HM Revenue & Customs" })).toBeVisible();
   });
 });

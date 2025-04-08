630579597397ce7c5c43c37b012ce1a4578cf8a7
Sandya25design <Sandya.karra@transformuk.com>
2025-04-08 13:39:57 +0100

    HMRC-Nav quota search

---
+++ tests/adminUI-search-refs.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+import LoginPage from '../pages/loginPage.js';
+
+test.describe("Search References", () => {
+  test.beforeEach(async ({ page }, testInfo) => await new LoginPage(process.env.ADMIN_URL, page, testInfo, true).login());
+
+  test("should display UK search references", async ({ page }) => {
+    await page.getByRole("link", { name: "Search references" }).click();
+    await page.getByRole("link", { name: "Search references" }).click();
+    await page.getByRole("link", { name: "to 5" }).click();
+    await page.getByRole("link", { name: "to 6" }).click();
+    await page.getByRole("link", { name: "Commodities in 0101" }).click();
+
+    await expect(page.getByRole("heading", { name: "Commodities search references" })).toBeVisible();
+  });
+
+  test("should display XI Search references", async ({ page }) => {
+    await page.getByRole("link", { name: "Switch to XI service" }).click();
+    await page.getByRole("link", { name: "Search references" }).click();
+    await page.getByRole("link", { name: "to 5" }).click();
+    await page.getByRole("link", { name: "to 6" }).click();
+    await page.getByRole("link", { name: "Commodities in 0101" }).click();
+
+    await expect(page.getByRole("heading", { name: "Commodities search references" })).toBeVisible();
+  });
+});
---
+++ tests/quota-search.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+import LoginPage from '../pages/loginPage.js';
+
+test.describe("Search for quotas", () => {
+  test("Validate UK quota Search Results", async ({ page }, testInfo) => {
+    await new LoginPage("/quota_search", page, testInfo).login();
+
+    await expect(page.getByRole('heading', { name: 'Search for quotas' })).toBeVisible();
+    await page.getByRole('textbox', { name: 'Enter the 6-digit quota order' }).fill('052016');
+    //Select a country to which the quota applies
+    await page.getByRole('combobox', { name: 'Select a country to which the' }).click();
+    await page.getByRole("option", { name: "Albania (AL)" }).click();
+    //search for quotas
+    await page.getByRole('button', { name: 'Search for quotas' }).click();
+    //Validation
+    await expect(page.getByRole("heading", { name: "Quota search results" })).toBeVisible();
+    await expect(page.getByRole("columnheader", { name: "Order number" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Opens in a popup" })).toHaveText("052016");
+  })
+});

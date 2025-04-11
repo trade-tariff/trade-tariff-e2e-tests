3edf930ca40609cf30afb3b1c8e0934f7955903e
William Fish <william.michael.fish@gmail.com>
2025-04-11 15:41:34 +0100

    HMRC-955: Create simple api wrapper function

--- tests/sampleapitest.spec.js
+++ tests/sampleapitest.spec.js
@@ -1,14 +1,12 @@
-import { test } from "@playwright/test";
-import { validate } from "../utils/validateSchema";
+import { test, expect } from "@playwright/test";
+import { validateApi } from "../utils/validateApi";
 
-test("GET /commodities/{id} returns valid JSON:API response", async ({
-  request,
-}) => {
-  const commodityId = "0101210000";
-  const res = await request.get(
-    `https://www.trade-tariff.service.gov.uk/api/v2/commodities/${commodityId}`
-  );
-  const json = await res.json();
-
-  validate(json);
+test("GET /commodities/{id} returns valid JSON:API response", async ({ request }) => {
+  let result;
+  result = await validateApi(request, "/api/v2/commodities/7013100000");
+  expect(result).toBeDefined();
+  result = await validateApi(request, "/api/v2/headings/0101");
+  expect(result).toBeDefined();
+  result = await validateApi(request, "/api/v2/chapters/01");
+  expect(result).toBeDefined();
 });

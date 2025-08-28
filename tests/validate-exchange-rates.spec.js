1526a9025608c53a16324777e20a181bcde3f210
Sandy25design <Sandya.karra@transformuk.com>
2025-08-28 09:46:12 +0100

    #Added extra wait time

--- tests/validate-exchange-rates.spec.js
+++ tests/validate-exchange-rates.spec.js
@@ -7,10 +7,9 @@ test.describe("Exchange Rates", () => {
     await new LoginPage("/exchange_rates", page).login();
     await page.locator('a[title^="View"]').first().click();
     await expect(
-      page.getByRole("columnheader", { name: "Country/territory" }),
-    ).toBeVisible();
-    await page.getByRole("link", { name: "Monthly exchange rates" }).click();
-
+    page.getByRole("columnheader", { name: "Country/territory" }),
+    ).toBeVisible({timeout: 20000});
+    await page.getByRole("link", { name: "Monthly exchange rates" }).click({timeout: 20000});
     await DownloadHelper.downloadAndVerify(
       page,
       page.getByRole("link", { name: /CSV\s+\d+\.?\d*\s*KB/ }).first(),
@@ -25,9 +24,8 @@ test.describe("Exchange Rates", () => {
     await page.locator('a[title^="View"]').first().click();
     await expect(
       page.getByRole("columnheader", { name: "Country/territory" }),
-    ).toBeVisible();
-    await page.getByRole("link", { name: "Average exchange rates" }).click();
-
+    ).toBeVisible({timeout: 20000});
+    await page.getByRole("link", { name: "Average exchange rates" }).click({timeout: 20000});
     await DownloadHelper.downloadAndVerify(
       page,
       page.getByRole("link", { name: /CSV\s+\d+\.?\d*\s*KB/ }).first(),
@@ -42,9 +40,8 @@ test.describe("Exchange Rates", () => {
     await page.locator('a[title^="View"]').first().click();
     await expect(
       page.getByRole("columnheader", { name: "Country/territory" }),
-    ).toBeVisible();
+    ).toBeVisible({timeout: 20000});
     await page.getByRole("link", { name: "Spot exchange rates" }).click();
-
     await DownloadHelper.downloadAndVerify(
       page,
       page.getByRole("link", { name: /CSV\s+\d+\.?\d*\s*KB/ }).first(),

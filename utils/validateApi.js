529a1c05f4537956659c5aa58159926d6af91a5e
Sandy25design <Sandya.karra@transformuk.com>
2025-08-29 15:49:47 +0100

    HMRC-1497: Adds spimm endpoint validation

--- utils/validateApi.js
+++ utils/validateApi.js
@@ -1,7 +1,7 @@
 import { validate } from "../utils/validateSchema";
 
-export async function validateApi(request, path) {
-  const res = await request.get(path);
+export async function validateApi(request, path, headers = {}) {
+  const res = await request.get(path, { headers });
   const json = await res.json();
 
   return validate(json);

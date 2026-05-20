8ee45a63e35aa45422ae7fc4b07068ba25a2dc48
William Fish <william.michael.fish@gmail.com>
2026-05-20 09:42:44 +0100

    BAU: Update stale e2e checks

--- tests/navigation-a-to-z-tariff.spec.js
+++ tests/navigation-a-to-z-tariff.spec.js
@@ -5,11 +5,11 @@ test.describe("A-Z Navigation", () => {
   test("Validating a-z navigation of the tariff", async ({ page }) => {
     await new LoginPage("/a-z-index/a", page).login();
     await page
-      .getByRole("link", { name: "Aa batteries (code: 8506)", exact: true })
+      .getByRole("link", { name: "Abaci (abacus) (code: 9503)", exact: true })
       .click();
 
     await expect(
-      page.getByRole("heading", { name: "Heading 8506 - Primary cells" }),
+      page.getByRole("heading", { name: "Heading 9503 - Tricycles" }),
     ).toBeVisible();
   });
 });

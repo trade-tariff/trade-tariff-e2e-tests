2aab9ae5ea95f06b0eebf4d4ca2040e1f7f11327
William Fish <william.michael.fish@gmail.com>
2026-06-18 16:10:54 +0100

    chore: manage pre-commit hooks with Nix
    Issue: No ticket/issue

--- flake.nix
+++ flake.nix
@@ -2,33 +2,117 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+    };
   };
 
   outputs =
     {
-      self,
       nixpkgs,
       flake-utils,
+      pre-commit-hooks,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs {
-          system = system;
+          inherit system;
           config.allowUnfree = true;
+        };
+
+        preCommitCheck = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          configPath = ".pre-commit-config-nix.yaml";
+          default_stages = [ "pre-commit" ];
+          hooks = {
+            check-added-large-files = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-case-conflicts = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-json = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-merge-conflicts = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-yaml = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            deadnix = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            detect-private-keys = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            end-of-file-fixer = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            eslint = {
+              enable = true;
+              settings.binPath = "./node_modules/.bin/eslint";
+              stages = [ "pre-commit" ];
+            };
+            markdownlint = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            nixfmt-rfc-style = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            prettier = {
+              enable = true;
+              settings.binPath = "./node_modules/.bin/prettier";
+              settings.list-different = false;
+              stages = [ "pre-commit" ];
+            };
+            pretty-format-json = {
+              enable = true;
+              settings.autofix = true;
+              stages = [ "pre-commit" ];
+            };
+            statix = {
+              enable = true;
+              settings.ignore = [ ".direnv" ];
+              stages = [ "pre-commit" ];
+            };
+            trim-trailing-whitespace = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            trufflehog = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+          };
         };
       in
       {
         devShells.default = pkgs.mkShell {
           shellHook = ''
             export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers.outPath};
+            ${preCommitCheck.shellHook}
           '';
 
-          buildInputs = with pkgs; [
-            playwright-driver.browsers
-            nodejs_latest
-            yarn
-          ];
+          buildInputs =
+            preCommitCheck.enabledPackages
+            ++ (with pkgs; [
+              playwright-driver.browsers
+              nodejs_latest
+              yarn
+            ]);
         };
       }
     );

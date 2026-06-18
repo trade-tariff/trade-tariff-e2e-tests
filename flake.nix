e64dacef16067fa569f3dc11b6d2970e6999128f
William Fish <william.michael.fish@gmail.com>
2026-06-18 16:35:20 +0100

    chore: route manual pre-commit through Nix config
    Issue: No ticket/issue

--- flake.nix
+++ flake.nix
@@ -104,6 +104,29 @@
           shellHook = ''
             export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers.outPath};
             ${preCommitCheck.shellHook}
+            export PATH=${pkgs.writeShellScriptBin "pre-commit" ''
+              set -euo pipefail
+
+              has_config=false
+              for arg in "$@"; do
+                case "$arg" in
+                  -c|--config|--config=*)
+                    has_config=true
+                    ;;
+                esac
+              done
+
+              if [ "$has_config" = true ]; then
+                exec ${preCommitCheck.config.package}/bin/pre-commit "$@"
+              fi
+
+              if [ "''${1:-}" = "run" ]; then
+                shift
+                exec ${preCommitCheck.config.package}/bin/pre-commit run --config .pre-commit-config-nix.yaml "$@"
+              fi
+
+              exec ${preCommitCheck.config.package}/bin/pre-commit "$@"
+            ''}/bin:$PATH
           '';
 
           buildInputs =

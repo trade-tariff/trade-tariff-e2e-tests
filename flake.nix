31de1ac285e18a8344d6ba7f261c27ae6d5af359
William Fish <william.michael.fish@gmail.com>
2026-06-18 16:43:34 +0100

    chore: pin nixfmt hook package
    Issue: No ticket/issue

--- flake.nix
+++ flake.nix
@@ -69,6 +69,7 @@
               stages = [ "pre-commit" ];
             };
             nixfmt-rfc-style = {
+              package = pre-commit-hooks.inputs.nixpkgs.legacyPackages.${system}.nixfmt;
               enable = true;
               stages = [ "pre-commit" ];
             };
